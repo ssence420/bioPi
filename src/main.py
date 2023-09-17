dc41b1005e758c6c0ce33ca301e38ad1b51fefa9
ssence420 <145261623+ssence420@users.noreply.github.com>
2023-09-17 19:28:10 +0200

    webapp update in main

--- src/main.py
+++ src/main.py
@@ -1,26 +1,12 @@
 import time
 import datetime
 import DATA
-from flask import Flask, render_template
+import webapp
 
 log_datei = 'log.txt'
 
+webapp.StartWeb()
 
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    # Lesen Sie den Inhalt der log.txt Datei
-    with open('log.txt', 'r') as file:
-        log_inhalt = file.read()
-
-    # Rendern Sie eine einfache HTML-Seite mit dem Inhalt
-    return f'<html><body><pre>{log_inhalt}</pre></body></html>'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
 
 # Funktion zum Schreiben der Messwerte in das Log
 def schreibe_log(datei, messwert):
