ed3a53fb47f0982ddf14e4e57bacec78886848e2
ssence420 <145261623+ssence420@users.noreply.github.com>
2023-09-17 20:38:32 +0200

    Update webapp.py

--- src/webapp.py
+++ src/webapp.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, Response
 
-
+import time
+import DATA
 
 
 app = Flask(__name__)
@@ -12,8 +13,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Lesen Sie den Inhalt der log.txt Datei
-    with open('log.txt', 'r') as file:
-        log_inhalt = file.read()
+    # with open('log.txt', 'r') as file:
+    #     log_inhalt = file.read()
+     
+    data = DATA.GetMeasures()
+    cTemp = data[0]
+    humidity = data[1]
+
+    yield f"data: Temperature: {cTemp}, Humidity: {humidity}\n\n"
+    time.sleep(10)
+
 
     # Rendern Sie eine einfache HTML-Seite mit dem Inhalt
     return f'<html><body><pre>{log_inhalt}</pre></body></html>'
