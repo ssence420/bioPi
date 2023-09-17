490bef910892d461d969ef0dc19242aa18cfe75a
ssence420 <145261623+ssence420@users.noreply.github.com>
2023-09-17 15:35:41 +0200

    adsfg

--- bmeExamp/BME/BME280-master/Python/BME280.py
+++ bmeExamp/BME/BME280-master/Python/BME280.py
@@ -134,7 +134,7 @@ elif humidity < 0.0 :
     humidity = 0.0
 
 # Output data to screen
-print "Temperature in Celsius : %.2f C" %cTemp
-print "Temperature in Fahrenheit : %.2f F" %fTemp
-print "Pressure : %.2f hPa " %pressure
-print "Relative Humidity : %.2f %%" %humidity
+print ("Temperature in Celsius : %.2f C" %cTemp)
+print ("Temperature in Fahrenheit : %.2f F" %fTemp)
+print ("Pressure : %.2f hPa " %pressure)
+print ("Relative Humidity : %.2f %%" %humidity)
